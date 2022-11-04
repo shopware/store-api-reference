3902825fa8537111c079b43cdb8edf6a818dce03
Christian Rades <C.Rades@shopware.com>
2022-11-04 11:18:30 +0100

    Fix artifact download (#13)
    * Fix artifact download
    
    * Update add_descriptions.yml

--- .github/scripts/set_descriptions.sh
+++ .github/scripts/set_descriptions.sh
@@ -1,6 +1,6 @@
 #! /bin/env bash
 
-for k in $(yq '. | keys | .[]' override.yml); do
-    newDescription=$(yq ".\"$k\"" override.yml);
-    yq -iPo json "$k.description = \"${newDescription}\"" storeapi.json;
+for k in $(yq '. | keys | .[]' descriptions.yml); do
+    newDescription=$(yq ".\"$k\"" descriptions.yml);
+    yq  -I 4 -o json -iP "$k.description = \"${newDescription}\"" storeapi.json;
 done
